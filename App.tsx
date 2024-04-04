cece52942e854b874cf9bd6c9902eec744021c15
Juan Díaz <3674588+JuanoD@users.noreply.github.com>
2024-04-03 21:00:46 -0500

    gesture handler

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
+import "react-native-gesture-handler";
+
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigation from "@/navigation/Tab";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
